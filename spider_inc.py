457db7d1d803c4b970014cdd65c456ac3079520b
SHERRY <sherry.feng@duke.edu>
2018-08-27 23:10:05 +0100

    first commit

---
+++ spider_inc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+import requests
+import json
+import sys
+
+result = open("test.txt", 'w')
+sys.stdout = result
+
+r = requests.get('https://www.inc.com/inc5000list/json/inc5000_2017.json')
+
+company_list = json.loads(r.text)
+company_id_list = []
+company_base_url = 'https://www.inc.com/rest/inc5000company/inc5000-usa-2017/'
+
+
+for company in company_list:
+    company_id_list.append(company['id'])
+
+for id in company_id_list:
+    url = unicode(company_base_url) + unicode(id)
+    r = requests.get(url)
+    company_dict = json.loads(r.text)
+    industry_list = company_dict['inc5000industryObj']
+    print industry_list['ifi_industry']
+    print company_dict['ifc_company'],
+    print company_dict['ifc_ceo_name'],
+    print company_dict['ifc_ceo_email'],
+    print company_dict['ifc_ceo_phone']
+
+
